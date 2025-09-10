dc4e9987a00b7cbd307344c645b1529f0d488498
Park Youngmin <bym010312@naver.com>
2025-09-10 13:42:28 +0900

    숫자 증가, 감소 기능 구현

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState(10);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 30 }}>결과 : {number} </Text>
+      <View style={{ flexDirection: "row", gap: 10}}>
+        <Button title="+" onPress={() => setNumber(number + 1)}/>
+        <Button title="-" onPress={() => setNumber(number - 1)}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
