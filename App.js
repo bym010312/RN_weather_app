eeea1ac019e58186d220bb5cc13059c2e74366e2
Park Youngmin <bym010312@naver.com>
2025-09-11 15:48:54 +0900

    Flex Box 사용법

--- App.js
+++ App.js
@@ -3,45 +3,19 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import React, { useState } from "react";
 
 export default function App() {
-  const [number, setNumber] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>결과 : {number} </Text>
-      <View style={styles.btnGroup}>
-        <Button
-          title="증가"
-          onPress={() => {
-            setNumber(number + 1);
-          }}
-        />
-        <Button
-          title="감소"
-          onPress={() => {
-            setNumber(number - 1);
-          }}
-        />
-      </View>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "red" }}></View>
+      <View
+        style={{ flex: 1.5, backgroundColor: "darkorange" }}
+      ></View>
+      <View
+        style={{ flex: 2, backgroundColor: "green" }}
+      ></View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 30,
-    color: "white",
-    fontWeight: "bold",
-  },
-  btnGroup: {
-    flexDirection: "row",
-    gap: 20,
-    borderWidth: 2,
-    marginTop: 20,
-  },
-});
+const styles = StyleSheet.create({});
+
