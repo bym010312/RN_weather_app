f0df921b0ea664db33e93408d7ab03ff9edc5ef9
Park Youngmin <bym010312@naver.com>
2025-09-11 11:42:30 +0900

    StatusBar 사용법

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
   const [number, setNumber] = useState(10);
   return (
     <View style={styles.container}>
-      <Text style={{...styles.text, color: "dodgerblue"}}>결과 : {number} </Text>
-      <Text style={{...styles.text}}>결과 : {number} </Text>
-      <View style={{ flexDirection: "row", gap: 10}}>
-        <Button title="+" onPress={() => setNumber(number + 1)}/>
-        <Button title="-" onPress={() => setNumber(number - 1)}/>
+      <Text style={{ ...styles.text, color: "dodgerblue" }}>
+        결과 : {number}{" "}
+      </Text>
+      <Text style={{ ...styles.text }}>결과 : {number} </Text>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Button title="+" onPress={() => setNumber(number + 1)} />
+        <Button title="-" onPress={() => setNumber(number - 1)} />
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -20,13 +22,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     color: "red",
     fontSize: 30,
-    fontWeight: "bold"
-  }
+    fontWeight: "bold",
+  },
 });
