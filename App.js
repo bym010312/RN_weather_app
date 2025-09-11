43a58f44f2a2e4bc8653b8538a7a80c717382d5f
Park Youngmin <bym010312@naver.com>
2025-09-11 16:36:42 +0900

    날씨 앱 레이아웃 구성 및 디자인

--- App.js
+++ App.js
@@ -4,18 +4,74 @@ import React, { useState } from "react";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "red" }}></View>
-      <View
-        style={{ flex: 1.5, backgroundColor: "darkorange" }}
-      ></View>
-      <View
-        style={{ flex: 2, backgroundColor: "green" }}
-      ></View>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={styles.cityCon}>
+        <Text style={styles.city}>서울</Text>
+      </View>
+      <View style={styles.weatherCon}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
+          <Text style={styles.desc}>맑음</Text>
+        </View>
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>24º</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffe01a",
+  },
+  cityCon: {
+    flex: 0.3,
+  },
+  city: {
+    flex: 1,
+    marginTop: 50,
+    paddingTop: 20,
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  weatherCon: {
+    flex: 1,
+  },
+  day: {
+    flex: 0.2,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
+  regDate: {
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 15,
+    backgroundColor: "black",
+    color: "white",
+    fontWeight: "bold",
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  desc: {
+    flex: 1.5,
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
 
+  },
+  tempCon: {
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  temp: {
+    textAlign: "center",
+    fontSize: 120,
+  },
+});
