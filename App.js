bc02b38ac1857d0492393b14b46a98bf1aceb2bd
Park Youngmin <bym010312@naver.com>
2025-09-11 12:04:32 +0900

    간단한 숫자, 증가 감소 UI 디자인 구현

--- App.js
+++ App.js
@@ -3,18 +3,25 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import React, { useState } from "react";
 
 export default function App() {
-  const [number, setNumber] = useState(10);
+  const [number, setNumber] = useState(0);
   return (
     <View style={styles.container}>
-      <Text style={{ ...styles.text, color: "dodgerblue" }}>
-        결과 : {number}{" "}
-      </Text>
-      <Text style={{ ...styles.text }}>결과 : {number} </Text>
-      <View style={{ flexDirection: "row", gap: 10 }}>
-        <Button title="+" onPress={() => setNumber(number + 1)} />
-        <Button title="-" onPress={() => setNumber(number - 1)} />
+      <Text style={styles.text}>결과 : {number} </Text>
+      <View style={styles.btnGroup}>
+        <Button
+          title="증가"
+          onPress={() => {
+            setNumber(number + 1);
+          }}
+        />
+        <Button
+          title="감소"
+          onPress={() => {
+            setNumber(number - 1);
+          }}
+        />
       </View>
-      <StatusBar style="auto"/>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -27,8 +34,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
-    color: "red",
     fontSize: 30,
+    color: "white",
     fontWeight: "bold",
+  },
+  btnGroup: {
+    flexDirection: "row",
+    gap: 20,
+    borderWidth: 2,
+    marginTop: 20,
   },
 });
