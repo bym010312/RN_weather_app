9a8630d118918af13d3da58bc243b35a793e27a7
Park Youngmin <bym010312@naver.com>
2025-09-12 21:49:59 +0900

    위도와 경도를 기반으로 현재 주소 가져오기

--- App.js
+++ App.js
@@ -1,61 +1,88 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Dimensions } from "react-native";
+import * as Location from "expo-location";
+
+// const {width: SCREEN_WIDTH, height} = Dimensions.get('window');
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [city,setCity] = useState(null);
+
+  //허가 여부
+  const [permitted, setPermitted] = useState(true);
+  const locationDate = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    if (!granted) {
+      setPermitted(false);
+      setErrorMsg("위치에 대한 권한 부여가 거부되었습니다.");
+
+      return;
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    // console.log(latitude);
+    // console.log(longitude);
+
+    const address = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGooglMaps: false });
+    // console.log(address)
+    // console.log(address[0].city)
+
+    const cityAddress = address[0].city;
+    setCity(cityAddress);
+
+  };
+  useEffect(() => {
+    locationDate();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.cityCon}>
-        <Text style={styles.city}>서울</Text>
+        <Text style={styles.city}>{city}</Text>
       </View>
-      <ScrollView horizontal contentContainerStyle={styles.weather}>
-      <View style={styles.weatherInner}>
-        <View style={styles.day}>
-          <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
-          <Text style={styles.desc}>맑음</Text>
-        </View>
-        <View style={styles.tempCon}>
-          <Text style={styles.temp}>24º</Text>
-        </View>
+      <View style={styles.regDateCon}>
+        <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
       </View>
-      <View style={styles.weatherCon}>
-        <View style={styles.day}>
-          <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
-          <Text style={styles.desc}>맑음</Text>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.weatherInner}>
+          <View style={styles.day}>
+            <Text style={styles.desc}>맑음</Text>
+          </View>
+          <View style={styles.tempCon}>
+            <Text style={styles.temp}>24º</Text>
+          </View>
         </View>
-        <View style={styles.tempCon}>
-          <Text style={styles.temp}>24º</Text>
+        <View style={styles.weatherInner}>
+          <View style={styles.day}>
+            <Text style={styles.desc}>맑음</Text>
+          </View>
+          <View style={styles.tempCon}>
+            <Text style={styles.temp}>24º</Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.weatherCon}>
-        <View style={styles.day}>
-          <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
-          <Text style={styles.desc}>맑음</Text>
+        <View style={styles.weatherInner}>
+          <View style={styles.day}>
+            <Text style={styles.desc}>맑음</Text>
+          </View>
+          <View style={styles.tempCon}>
+            <Text style={styles.temp}>24º</Text>
+          </View>
         </View>
-        <View style={styles.tempCon}>
-          <Text style={styles.temp}>24º</Text>
-        </View>
-      </View>
-      <View style={styles.weatherCon}>
-        <View style={styles.day}>
-          <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
-          <Text style={styles.desc}>맑음</Text>
-        </View>
-        <View style={styles.tempCon}>
-          <Text style={styles.temp}>24º</Text>
-        </View>
-      </View>
-            <View style={styles.weatherCon}>
-        <View style={styles.day}>
-          <Text style={styles.regDate}>9월 11일, 목, 14:20</Text>
-          <Text style={styles.desc}>맑음</Text>
-        </View>
-        <View style={styles.tempCon}>
-          <Text style={styles.temp}>24º</Text>
-        </View>
-      </View>
       </ScrollView>
-
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -72,24 +99,15 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 50,
     paddingTop: 20,
-    fontSize: 30,
+    fontSize: 40,
     textAlign: "center",
     fontWeight: "bold",
   },
-  weather: {
-    backgroundColor: "blue",
-  },
-  weatherInner: {
-    flex: 1,
-  },
-  day: {
-    flex: 0.2,
-    textAlign: "center",
+  regDateCon: {
     alignItems: "center",
-    justifyContent: "center",
-    
   },
   regDate: {
+    marginTop: 30,
     paddingTop: 10,
     paddingLeft: 20,
     paddingRight: 20,
@@ -100,17 +118,26 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     overflow: "hidden",
   },
+  weather: {},
+  weatherInner: {
+    flex: 3,
+    width: SCREEN_WIDTH,
+  },
+  day: {
+    flex: 0.2,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   desc: {
     flex: 1.5,
     marginTop: 20,
     fontSize: 25,
     fontWeight: "bold",
-
   },
   tempCon: {
-    flex: 0.3,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   temp: {
     textAlign: "center",
